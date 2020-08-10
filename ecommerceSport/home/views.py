3c4b98ce8763b09351f1c0ca7fccfc0b007ecff2
sijanghm <sijan.ghimire29@gmail.com>
2020-08-10 17:38:33 +0545

    bug fixed

--- ecommerceSport/home/views.py
+++ ecommerceSport/home/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return  render(request, 'index.html',{})
+    return  render(request, 'index.html')
 
 
     
